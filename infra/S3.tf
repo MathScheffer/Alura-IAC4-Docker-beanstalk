1b6eb70402d31ef45b8e34243c2aa7184eacae35
Matheus Martins Scheffer <matheus.scheffer@dbccompany.com.br>
2023-03-26 13:12:15 -0300

    modificar

--- infra/S3.tf
+++ infra/S3.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_object" "docker" {
     aws_s3_bucket.beanstalk_deploy
   ]
   bucket = "matty-us-east-1-123456789-beanstalk-deploy-${var.nome}-deploys"
-  key    = "producao.zip"
-  source = "producao.zip"
-  etag = filemd5("producao.zip")
+  key    = "prod.zip"
+  source = "prod.zip"
+  etag = filemd5("prod.zip")
 }
